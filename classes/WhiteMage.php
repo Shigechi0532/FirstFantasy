224675a1c4e281025627024651f4e01e95e2ce40
Shigechi0532 <130873801+Shigechi0532@users.noreply.github.com>
2024-03-09 20:40:45 +0900

    白魔導士クラス作成
    白魔導士クラス作成
    回復用メソッド(recavaryDamage)作成

--- classes/WhiteMage.php
+++ classes/WhiteMage.php
@@ -1,25 +1,27 @@
 <?php
-class BlackMage extends Human
+class WhiteMage extends Human
 {
     const MAX_HITPOINT = 80;
     private $hitPoint = 80;
     private $attackPoint = 10;
-    private $intelligence = 30; //魔法攻撃力
+    private $intelligence = 30;
 
     public function __construct($name)
     {
         parent::__construct($name, $this->hitPoint, $this->attackPoint);
     }
 
-    public function doAttack($enemy){
+    public function doAttackWhiteMage($enemy,$human)
+    {
         if(rand(1,2) === 1){
             echo "『" .$this->getName() ."』のスキルが発動した！\n";
-            echo "『ファイア』！！\n";
-            echo $enemy->getName() ."に". $this->intelligence * 1.5. "のダメージ！\n";
-            $enemy->tookDamage($this->intelligence * 1.5);
+            echo "『ケアル』！！\n";
+            echo $human->getName() ."『のHPを" .$this->intelligence * 1.5 ."回復！\n";
+            $human->recoveryDamage($this->intelligence * 1.5. $human);
         }else{
             parent::doAttack($enemy);
         }
         return true;
     }
 }
+?>
